3c2a443cc2a8fccc93cefaa714e9b8f2614a2f02
Hosam <hossamnasser938@gmail.com>
2018-12-11 21:37:59 +0200

    activity is done

--- app/src/main/java/com/example/android/quizzy/activity/LoginActivity.kt
+++ app/src/main/java/com/example/android/quizzy/activity/LoginActivity.kt
@@ -3,13 +3,16 @@ package com.example.android.quizzy.activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.Window
 import com.example.android.quizzy.R
 import com.example.android.quizzy.fragment.LoginFragment
 
-class LoginActivity : AppCompatActivity(), LoginFragment.LoginTransitionInterface {
+class LoginActivity : AppCompatActivity() , LoginFragment.LoginTransitionInterface{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_login)
         openFragment(LoginFragment())
     }
