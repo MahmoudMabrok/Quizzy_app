4d0797cdf9d1abb17aff746065855cd03784f5aa
Hosam <hossamnasser938@gmail.com>
2018-12-12 17:05:34 +0200

    Make sure to clear back stack when moving from WalkThrough to Login

--- app/src/main/java/com/example/android/quizzy/activity/WalkThroughActivty.java
+++ app/src/main/java/com/example/android/quizzy/activity/WalkThroughActivty.java
@@ -50,7 +50,7 @@ public class  WalkThroughActivty extends FancyWalkthroughActivity {
     public void onFinishButtonPressed() {
         //Navigate to Login Activity
         Intent intent = new Intent(this, LoginActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
     }
 }
