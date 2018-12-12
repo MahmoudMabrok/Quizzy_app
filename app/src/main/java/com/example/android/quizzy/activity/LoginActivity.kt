a5276ff08f8571a404062bc8f50fff40036f5757
Hosam <hossamnasser938@gmail.com>
2018-12-12 16:47:59 +0200

    Use the correct Activity in openFragment method

--- app/src/main/java/com/example/android/quizzy/activity/LoginActivity.kt
+++ app/src/main/java/com/example/android/quizzy/activity/LoginActivity.kt
@@ -3,11 +3,14 @@ package com.example.android.quizzy.activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.Window
 import com.example.android.quizzy.R
 import com.example.android.quizzy.fragment.LoginFragment
 
 class LoginActivity : AppCompatActivity() , LoginFragment.LoginTransitionInterface{
+
+    private val TAG = "LoginActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,13 +22,15 @@ class LoginActivity : AppCompatActivity() , LoginFragment.LoginTransitionInterfa
 
     override fun openFragment(caller: Any, fragment: Fragment) {
         //when opening the LoginFragment from MainActivity, do not add to BackStack
-        if(fragment is LoginFragment && caller is MainActivity){
+        if(fragment is LoginFragment && caller is LoginActivity){
+            Log.d(TAG, "open LoginFragment from LoginActivity")
             supportFragmentManager
                     .beginTransaction()
                     .replace(R.id.login_frame_layout, fragment)
                     .commit()
         }
         else {
+            Log.d(TAG, "open fragment other than LoginFragment")
             supportFragmentManager
                     .beginTransaction()
                     .addToBackStack(null)
